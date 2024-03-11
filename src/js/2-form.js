e7765038475de35148f1026e3113d732df3e004b
alinamoiseichuk <148059615+alinamoiseichuk@users.noreply.github.com>
2024-03-11 12:25:29 +0100

    form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,8 +4,8 @@ const form = document.querySelector(".feedback-form");
 const input = form.querySelector("input");
 const textarea = form.querySelector("textarea");
 
+
 form.addEventListener("input", event => {
-    event.preventDefault();
     const email = input.value.trim();
     const message = textarea.value.trim();
     const data = JSON.stringify({ email, message });
@@ -16,11 +16,22 @@ form.addEventListener("submit", event => {
     event.preventDefault();
     const email = input.value.trim();
     const message = textarea.value.trim();
+
     if (email === "" || message === "") {
         return alert("Будь-ласка, заповніть всі поля форми!")
     }
-    console.log(`email: ${email}, message: ${message}`);
+    const info = {
+        email,
+        message,
+    }
+    console.log(info);
     localStorage.removeItem(STORAGE_KEY);
     form.reset();
 });
 
+const jsn = localStorage.getItem(STORAGE_KEY);
+const saved = JSON.parse(jsn);console.log(saved);
+if (saved !== null) {
+    input.value = saved.email;
+    textarea.value = saved.message;
+}
